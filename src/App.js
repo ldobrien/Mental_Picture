315ac0df05e04be1a0387967754140c0e304c80a
unknown <johnsr12@mcmaster.ca>
2018-11-24 17:06:31 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,30 +6,55 @@ import Activity from './components/Activity';
 import { VictoryContainer } from 'victory'
 
 
+
 class App extends Component {
   render() {
     return (
+      
+      <div>
+      <head >
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        </ link>
+        <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+        </ link>
+      </ head>
+
       <div className="App">
-        <header className="App-header center green">
-          WACK HESTERN
-        </header>
+      <nav>
+    <div class="nav-wrapper green darken-4">
+      <a href="#" class="brand-logo center">Mental Picture</a>
+    </div>
+  </nav>
+
+      <div class="card-panel yellow lighten-4">
         <div class="row">
         <div class="col s12 m6">
+        <div class="card-panel blue lighten-5">
         <Mood />
         </div>
+        </div>
         <div class="col s12 m6">
+        <div class="card-panel blue lighten-5">
         <Sleep />
         </div>
         </div>
+        </div>
 
+     
         <div class="row">
           <div class="col s12 m6">
+          <div class="card-panel blue lighten-5">
             <Diet/>
+          </ div>
           </div>
           <div class="col s12 m6">
+          <div class="card-panel blue lighten-5">
             <Activity/>
+          </ div>
           </div>
         </div>
+      </div>
+      </div>
       </div>
     );
   }
