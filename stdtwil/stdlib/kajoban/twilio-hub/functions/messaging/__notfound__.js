adc9f98e1d8cccf08290349fec155f97bcd01121
Kajoban Kuhaparan <kuhapark@mcmaster.ca>
2018-11-24 07:52:23 -0500

    im going to take a nap, bye

--- stdtwil/stdlib/kajoban/twilio-hub/functions/messaging/__notfound__.js
+++ stdtwil/stdlib/kajoban/twilio-hub/functions/messaging/__notfound__.js
@@ -1,11 +1,10 @@
 /**
 * Not found handler - handles all SMS / MMS that don't match a command
 */
-
 module.exports = (tel = '', body = '', media = null, from = {}, to = {}, callback) => {
 
 	console.log(body)
-	
+
 	return callback(
 		null,
 		`\nThank you for your response!`
