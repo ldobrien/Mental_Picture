9dd761ac340bbd57753858e9a0b840c303b0187c
Kajoban Kuhaparan <kuhapark@mcmaster.ca>
2018-11-25 01:06:42 -0500

    some data changes

--- stdtwil/stdlib/kajoban/twilio-hub/helpers/fakeData.js
+++ stdtwil/stdlib/kajoban/twilio-hub/helpers/fakeData.js
@@ -30,12 +30,12 @@ function fake(){
             let diet = Math.floor(Math.random() * (2500 - 0) + 0);
             let exercise = Math.floor(Math.random() * (4 - 0) + 0);
             let sleep = Math.floor(Math.random() * (12 - 0) + 0);
-            let mood = Math.floor(Math.random() * (5 - 0) + 0);
+            let mood = Math.floor(Math.random() * (5 - 2) + 0);
 
             let json_obj = `{"date": ${JSON.stringify(date)}, "diet":${diet}, "exercise":${exercise}, "sleep":${sleep}, "mood":${mood}}`
-            //console.log(json_obj)
+            console.log(json_obj)
             
-            m.insertData(json_obj)
+            //m.insertData(json_obj)
         }
     }
 
